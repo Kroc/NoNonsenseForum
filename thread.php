b9762e82b221e12eb08f0c08a9b78dcef7499e96
Kroc Camen <kroc@camendesign.com>
2012-12-24 10:12:08 +0000

    Fix #160 "Placeholders and Overflow" Doesn't appear to be working for me on Firefox 17, but I'll leave it in, it will rectify itself in the future.

--- thread.php
+++ thread.php
@@ -108,6 +108,8 @@ if ($ID = (preg_match ('/^[A-Z0-9]+$/i', @$_GET['append']) ? $_GET['append'] : f
 	)) {
 		//append the given text to the reply
 		//(see 'theme.config.php' if it exists, otherwise 'theme.config.default.php' for `THEME_APPEND`)
+		//TODO: change THEME_APPEND to markup, instead of HTML, so that we can verify appended text for title ID
+		//      uniqueness by using `strip_tags` on the post text, adding the divider, the append, then format it
 		$post->description .= "\n".sprintf (THEME_APPEND,
 			safeHTML (NAME),		//author
 			gmdate ('r', time ()),		//machine-readable time
